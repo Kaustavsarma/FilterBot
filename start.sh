a576c5e3ebde3a7f849b155a0d1fc058496b733d
Kaustavsarma <115160080+Kaustavsarma@users.noreply.github.com>
2022-12-23 16:18:20 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Kaustavsarma/FilterBot.git /FilterBot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /FilterBot
+fi
+cd /FilterBot
+pip3 install -U -r requirements.txt
+echo "Starting Bot....💥"
+python3 bot.py
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
